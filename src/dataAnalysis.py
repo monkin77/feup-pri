502252504a105b7341bde5a9e5b8b3bc30775865
João Mesquita <monkinsmurf@gmail.com>
2022-10-04 12:16:58 +0100

    Add: Initial Data analysis

--- src/dataAnalysis.py
+++ src/dataAnalysis.py
@@ -18,7 +18,7 @@ happiness_params = company_data.happiness.tolist()
 happiness_params = [eval(x).get('Work Happiness Score', np.nan) for x in happiness_params]
 company_data['happiness'] = happiness_params
 company_data['happiness'] = pd.to_numeric(company_data['happiness'], errors='coerce')
-print(company_data['happiness'])
+# print(company_data['happiness'])
  
 # ======== Create 5 columns from the 'ratings' map ========
 ratings = company_data.ratings.tolist()
@@ -39,9 +39,40 @@ for key_idx in range(len(keys)):
 company_data = company_data.drop(['ratings'], axis=1)
 # print("company_data new columns: ", company_data.columns)
 
+# ======== See Correlation between features ========
+def show_all_correlation():
+    plt.figure()
+    sns.heatmap(company_data[['rating', 'ceo_approval', 'employees', 'revenue', 'happiness', 'compensation/benefits', 
+        'job_security/advancement', 'management', 'culture', 'work_life_balance']].corr(), xticklabels=True, yticklabels=True, vmin=-1.0, vmax=1.0)
 
-plt.figure()
-ax = sns.heatmap(company_data[['rating', 'ceo_approval', 'employees', 'revenue', 'happiness', 'compensation/benefits', 
-    'job_security/advancement', 'management', 'culture', 'work_life_balance']].corr(), xticklabels=True, yticklabels=True, vmin=-1.0, vmax=1.0)
+    plt.show()
 
-plt.show()
+
+# ======== Investigate correlation between company rating -> company size ========
+def show_employee_rating_correlation():
+    g = sns.catplot(x="employees", y="rating", data=company_data, kind="bar", palette="muted").set(title="Work Satisfaction V.S company size")
+    g.despine(left=True)
+    g = g.set_ylabels("Reviews Rating")
+    plt.show()
+
+
+# ======== Investigate correlation between company rating -> revenue ========
+def show_revenue_rating_correlation():
+    g = sns.catplot(x="revenue", y="rating", data=company_data, kind="bar", palette="muted").set(title="Work Satisfaction V.S company revenue")
+    g.despine(left=True)
+    g = g.set_ylabels("Reviews Rating")
+    plt.show()
+
+# ======== Correlation between rating factors ========
+def show_revenue_rating_correlation():
+    plt.figure()
+    sns.heatmap(company_data[['rating', 'ceo_approval', 'happiness', 'compensation/benefits', 
+        'job_security/advancement', 'management', 'culture', 'work_life_balance']].corr(), xticklabels=True, yticklabels=True, vmin=0.5, vmax=1.0)
+    plt.show()
+
+
+
+
+# ======== Call Method ========
+# show_all_correlation()
+show_revenue_rating_correlation()
