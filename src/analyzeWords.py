da732fb7ca7233500d5cf16fd39a55fbe7019705
up201905853 <up201905853@fe.up.pt>
2022-10-09 17:50:00 +0100

    Added company description wordcloud

--- src/analyzeWords.py
+++ src/analyzeWords.py
@@ -6,8 +6,14 @@ from utils import calcObjectMean
 # ======== read data set ========
 company_data = pd.read_csv("./assets/processed_reviews.csv")
 
-wordcloud2 = WordCloud().generate(company_data['roles'].to_string())
-plt.imshow(wordcloud2)
+description_wordcloud = WordCloud().generate(company_data['description'].to_string())
+plt.imshow(description_wordcloud)
+plt.axis("off")
+# plt.show()
+plt.savefig("./assets/images/wordcloud_description.png")
+
+roles_wordcloud = WordCloud().generate(company_data['roles'].to_string())
+plt.imshow(roles_wordcloud)
 plt.axis("off")
 # plt.show()
 plt.savefig("./assets/images/wordcloud_roles.png")
