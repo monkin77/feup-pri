2313535636c6a0f98275c9cd1d27b43ca4f8d312
up201905853 <up201905853@fe.up.pt>
2022-11-12 20:56:50 +0000

    Added query 4 results and metrics

--- solr/solrMetrics/q4/evalQ4.py
+++ solr/solrMetrics/q4/evalQ4.py
@@ -11,9 +11,9 @@ from sklearn.metrics import PrecisionRecallDisplay
 ### First step: read the qrels file and fetch query results from Solr's REST API 
 QRELS_FILE = "results.txt"
 # get here the url with edisMax
-QUERY_URL = ""
+QUERY_URL = "http://localhost:8983/solr/reviews/select?_=1668280615617&defType=edismax&fl=*+score&fq=employees:%5B8+TO+*%5D&indent=true&q=Financial+Technology&q.op=OR&qf=name+industry+description&rows=10&start=0"
 # get here the url with edisMax and weights
-QUERY_URL_WEIGHT = ""
+QUERY_URL_WEIGHT = "http://localhost:8983/solr/reviews/select?_=1668280615617&defType=edismax&fl=*+score&fq=employees:%5B8+TO+*%5D&indent=true&q=Financial%5E1.5+Technology%5E2&q.op=OR&qf=name+industry%5E2+description%5E1.5&rows=10&start=0"
 
 # Read qrels to extract relevant documents
 relevant = list(map(lambda el: el.strip(), open(QRELS_FILE).readlines()))
