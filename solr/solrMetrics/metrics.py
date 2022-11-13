6d56fd1fcf3ccb69d89c2da3355f5f52a88a2dd3
up201905853 <up201905853@fe.up.pt>
2022-11-13 15:31:28 +0000

    Added interpolated and no interpolated graphs for queries

--- solr/solrMetrics/metrics.py
+++ solr/solrMetrics/metrics.py
@@ -7,130 +7,176 @@ import pandas as pd
 import requests
 from sklearn.metrics import PrecisionRecallDisplay
 
+def get_metrics(USE_INTERPOLATION, QRELS_FILE, QUERY_URL, QUERY_URL_WEIGHT):
 
-### First step: read the qrels file and fetch query results from Solr's REST API 
-QRELS_FILE = "q1/results.txt"
-# get here the url with edisMax
-QUERY_URL = "http://localhost:8983/solr/reviews/select?_=1668191500840&defType=edismax&fl=name,+rating,+reviews,+description,+happiness*,+ratings*,+headquarters,+employees,+industry,+revenue,+custom_rating,+ceo*,+interview*,+locations*,+roles*,+salary*,+score&indent=true&q=telecommunication%0A%5B4+TO+5%5D&q.op=OR&qf=name+industry+rating&rows=20"
-# get here the url with edisMax and weights
-QUERY_URL_WEIGHT = "http://localhost:8983/solr/#/reviews/query?q=telecommunication%0A%5B4%20TO%205%5D&q.op=OR&defType=edismax&indent=true&qf=name%20industry%20rating&fl=name,%20rating,%20reviews,%20description,%20happiness*,%20ratings*,%20headquarters,%20employees,%20industry,%20revenue,%20custom_rating,%20ceo*,%20interview*,%20locations*,%20roles*,%20salary*,%20score&rows=20"
+    # Read qrels to extract relevant documents
+    relevant = list(map(lambda el: el.strip(), open(QRELS_FILE).readlines()))
 
-# Read qrels to extract relevant documents
-relevant = list(map(lambda el: el.strip(), open(QRELS_FILE).readlines()))
-# Get query results from Solr instance
-results = requests.get(QUERY_URL).json()['response']['docs']
+    # Get query results from Solr instance
+    results_normal = requests.get(QUERY_URL).json()['response']['docs']
 
-# get the results with weights
-results_weight = requests.get(QUERY_URL_WEIGHT).json()['response']['docs']
+    # get the results with weights
+    results_weight = requests.get(QUERY_URL_WEIGHT).json()['response']['docs']
 
-# get the results with no schema
-results_no_schema = json.load(open('noSchema.json', encoding="utf8"))['response']['docs']
+    # get the results with no schema
+    results_no_schema = json.load(open('noSchema.json', encoding="utf8"))['response']['docs']
 
-# In a second step, we calculate some common evaluation metrics that can be used to compare systems and to assess their performance.
-# METRICS TABLE
-# Define custom decorator to automatically calculate metric based on key
-metrics = {}
-def metric(f): return metrics.setdefault(f.__name__, f)
+    all_results = [results_normal, results_weight, results_no_schema]
 
-@metric
-def ap(results, relevant):
-  """Average Precision"""
+    _, ax = plt.subplots(figsize=(5, 5))
 
-  precision_values = [
-      len([
-          doc
-          for doc in results[:idx]
-          if doc['name'] in relevant
-      ]) / idx
-      for idx in range(1, len(results) + 1)
-  ]
-  
-  return sum(precision_values) / len(relevant)
+    colors = cycle(["red", "green",  "blue"])
 
-@metric
-def p10(results, relevant, n=10):
-  """Precision at N"""
-  return len([doc for doc in results[:n] if doc['name'] in relevant])/n
+    i = 0
+    for results, color in zip(all_results, colors):
 
-@metric
-def r10(results, relevant, n=10):
-  """Recall at N"""
-  return len([doc for doc in results[:n] if doc['name'] in relevant])/len(relevant)
+        # In a second step, we calculate some common evaluation metrics that can be used to compare systems and to assess their performance.
+        # METRICS TABLE
+        # Define custom decorator to automatically calculate metric based on key
+        metrics = {}
+        def metric(f): return metrics.setdefault(f.__name__, f)
 
-## check what is this
-@metric
-def ndcg(results, relevant):
-  """Normalized Discounted Cumulative Gain"""
+        @metric
+        def ap(results, relevant):
+            """Average Precision"""
 
-  dcg = 0
-  for idx, doc in enumerate(results):
-      if doc['name'] in relevant:
-          dcg += 1 / np.log2(idx + 2)
+            # we need to do this for because the results with no schema are different
+            relevant_index = []
+            index = 0
+            for res in results:
+                if (i != 2 and res['name'] in relevant) or (i == 2 and res['name'][0] in relevant):
+                    relevant_index.append(index)
+                index = index + 1
 
-  idcg = 0
-  for idx, doc in enumerate(relevant):
-      idcg += 1 / np.log2(idx + 2)
+            if len(relevant_index) == 0:
+                return 0
 
-  return dcg / idcg
+            precision_values = [
+                len([
+                    doc
+                    for doc in results[:idx]
+                    if (i != 2 and doc['name'] in relevant) or (i == 2 and doc['name'][0] in relevant)
+                ]) / idx
+                for idx in range(1, len(results) + 1)
+            ]
 
-def calculate_metric(key, results, relevant):
-  return metrics[key](results, relevant)
+            precision_sum = 0
+            for idx in relevant_index:
+                precision_sum = precision_sum + precision_values[idx]
+            
+            return precision_sum / len(relevant_index)
 
-# Define metrics to be calculated
-evaluation_metrics = {
-    'ap': 'Average Precision',
-    'p10': 'Precision at 10 (P@10)',
-    'r10': 'Recall at 10 (R@10)',
-    'ndcg': 'Normalized Discounted Cumulative Gain (NDCG)'
-}
+        @metric
+        def p10(results, relevant, n=10):
+            if len(results) == 0:
+                return 0
 
-# Calculate all metrics and export results as LaTeX table
-df = pd.DataFrame([['Metric','Value']] +
-    [
-        [evaluation_metrics[m], calculate_metric(m, results, relevant)]
-        for m in evaluation_metrics
-    ]
-)
+            """Precision at N"""
+            return len([
+                doc 
+                for doc in results[:n] 
+                if (i != 2 and doc['name'] in relevant) or (i == 2 and doc['name'][0] in relevant)
+            ])/len(results)
 
-with open('q1/results.tex','w') as tf:
-    tf.write(df.to_latex())
+        @metric
+        def r10(results, relevant, n=10):
+            """Recall at N"""
+            return len([
+                doc 
+                for doc in results[:n] 
+                if (i != 2 and doc['name'] in relevant) or (i == 2 and doc['name'][0] in relevant)
+            ])/len(relevant)
 
-# In a third step, we calculate the Precision-Recall curve for the query results, to provide
-# a more visual clue about the systems' performance.
 
-# PRECISION-RECALL CURVE
-# Calculate precision and recall values as we move down the ranked list
-precision_values = [
-    len([
-        doc 
-        for doc in results[:idx]
-        if doc['name'] in relevant
-    ]) / idx 
-    for idx, _ in enumerate(results, start=1)
-]
+        def calculate_metric(key, results, relevant):
+            return metrics[key](results, relevant)
 
-recall_values = [
-    len([
-        doc for doc in results[:idx]
-        if doc['name'] in relevant
-    ]) / len(relevant)
-    for idx, _ in enumerate(results, start=1)
-]
+        # Define metrics to be calculated
+        evaluation_metrics = {
+            'ap': 'Average Precision',
+            'p10': 'Precision at 10 (P@10)',
+            'r10': 'Recall at 10 (R@10)',
+        }
 
-precision_recall_match = {k: v for k,v in zip(recall_values, precision_values)}
+        # Calculate all metrics and export results as LaTeX table
+        df = pd.DataFrame([['Metric','Value']] +
+            [
+                [evaluation_metrics[m], calculate_metric(m, results, relevant)]
+                for m in evaluation_metrics
+            ]
+        )
 
-# Extend recall_values to include traditional steps for a better curve (0.1, 0.2 ...)
-recall_values.extend([step for step in np.arange(0.1, 1.1, 0.1) if step not in recall_values])
-recall_values = sorted(set(recall_values))
-
-# Extend matching dict to include these new intermediate steps
-for idx, step in enumerate(recall_values):
-    if step not in precision_recall_match:
-        if recall_values[idx-1] in precision_recall_match:
-            precision_recall_match[step] = precision_recall_match[recall_values[idx-1]]
+        if i == 0:
+            filename = 'results_normal.tex'
+        elif i == 1:
+            filename = 'results_weight.tex'
         else:
-            precision_recall_match[step] = precision_recall_match[recall_values[idx+1]]
+            filename = 'results_no_schema.tex'
 
-disp = PrecisionRecallDisplay([precision_recall_match.get(r) for r in recall_values], recall_values)
-disp.plot()
-plt.savefig('q1/precision_recall.pdf')
+        with open(filename,'w') as tf:
+            tf.write(df.to_latex())
+
+        # In a third step, we calculate the Precision-Recall curve for the query results, to provide
+        # a more visual clue about the systems' performance.
+        # PRECISION-RECALL CURVE
+        precision_values = [
+            len([
+                doc 
+                for doc in results[:idx]
+                if (i != 2 and doc['name'] in relevant) or (i == 2 and doc['name'][0] in relevant)
+            ]) / idx 
+            for idx, _ in enumerate(results, start=1)
+        ]
+
+        recall_values = [
+            len([
+                doc for doc in results[:idx]
+                if (i != 2 and doc['name'] in relevant) or (i == 2 and doc['name'][0] in relevant)
+            ]) / len(relevant)
+            for idx, _ in enumerate(results, start=1)
+        ]
+
+        if len(precision_values) == 0 or len(recall_values) == 0:
+            continue
+
+        precision_recall_match = {k: v for k,v in zip(recall_values, precision_values)}
+
+        # Extend recall_values to include traditional steps for a better curve (0.1, 0.2 ...)
+        if USE_INTERPOLATION:
+            recall_values.extend([step for step in np.arange(0.1, 1.1, 0.1) if step not in recall_values])
+            recall_values = sorted(set(recall_values))
+
+        # Extend matching dict to include these new intermediate steps
+        for idx, step in enumerate(recall_values):
+            if step not in precision_recall_match:
+                if recall_values[idx-1] in precision_recall_match:
+                    precision_recall_match[step] = precision_recall_match[recall_values[idx-1]]
+                else:
+                    precision_recall_match[step] = precision_recall_match[recall_values[idx+1]]
+
+
+        disp = PrecisionRecallDisplay([precision_recall_match.get(r) for r in recall_values], recall_values)
+
+        if(i == 0):
+            disp.plot(ax=ax, name=f"Precision-recall w/ schema",
+                color=color, linewidth=1)
+        elif(i == 1):
+            disp.plot(ax=ax, name=f"Precision-recall w/ modifiers",
+                    color=color, linewidth=1.5)
+        elif(i == 2):
+            disp.plot(ax=ax, name=f"Precision-recall schemaless",
+                color=color, linewidth=1)
+        i += 1
+
+    # add the legend for the iso-f1 curves
+    handles, labels = disp.ax_.get_legend_handles_labels()
+
+    # set the legend and the axes
+    ax.legend(handles=handles, labels=labels, loc="best")
+
+    if USE_INTERPOLATION:
+        ax.set_title("Interpolated Precision-Recall curve")
+        plt.savefig('precision_recall_interpolation.png')
+    else:
+        ax.set_title("Precision-Recall curve")
+        plt.savefig('precision_recall.png')
--- solr/solrMetrics/q4/evalQ4.py
+++ solr/solrMetrics/q4/evalQ4.py
@@ -1,3 +1,11 @@
+import os
+import sys
+
+script_dir = os.path.dirname( __file__ )
+mymodule_dir = os.path.join( script_dir, '..')
+sys.path.append( mymodule_dir )
+from metrics import get_metrics
+
 import json
 from itertools import cycle
 
@@ -7,6 +15,7 @@ import pandas as pd
 import requests
 from sklearn.metrics import PrecisionRecallDisplay
 
+USE_INTERPOLATION = True
 
 ### First step: read the qrels file and fetch query results from Solr's REST API 
 QRELS_FILE = "relevantResults.txt"
@@ -15,165 +24,5 @@ QUERY_URL = "http://localhost:8983/solr/reviews/select?_=1668280615617&defType=e
 # get here the url with edisMax and weights
 QUERY_URL_WEIGHT = "http://localhost:8983/solr/reviews/select?_=1668280615617&defType=edismax&fl=*+score&fq=employees:%5B8+TO+*%5D&indent=true&q=Financial%5E1.5+Technology%5E2&q.op=OR&qf=name+industry%5E2+description%5E1.5&rows=10&start=0"
 
-# Read qrels to extract relevant documents
-relevant = list(map(lambda el: el.strip(), open(QRELS_FILE).readlines()))
-
-# Get query results from Solr instance
-results_normal = requests.get(QUERY_URL).json()['response']['docs']
-
-# get the results with weights
-results_weight = requests.get(QUERY_URL_WEIGHT).json()['response']['docs']
-
-# get the results with no schema
-results_no_schema = json.load(open('noSchema.json', encoding="utf8"))['response']['docs']
-
-results = [results_normal, results_weight, results_no_schema]
-
-_, ax = plt.subplots(figsize=(7, 8))
-
-colors = cycle(["red", "green",  "blue"])
-
-i = 0
-for results, color in zip(results, colors):
-
-    # In a second step, we calculate some common evaluation metrics that can be used to compare systems and to assess their performance.
-    # METRICS TABLE
-    # Define custom decorator to automatically calculate metric based on key
-    metrics = {}
-    def metric(f): return metrics.setdefault(f.__name__, f)
-
-    @metric
-    def ap(results, relevant):
-        """Average Precision"""
-
-        # we need to do this for because the results with no schema are different
-        relevant_index = []
-        index = 0
-        for res in results:
-            if (i != 2 and res['name'] in relevant) or (i == 2 and res['name'][0] in relevant):
-              relevant_index.append(index)
-            index = index + 1
-
-        if len(relevant_index) == 0:
-            return 0
-
-        precision_values = [
-            len([
-                doc
-                for doc in results[:idx]
-                if (i != 2 and doc['name'] in relevant) or (i == 2 and doc['name'][0] in relevant)
-            ]) / idx
-            for idx in range(1, len(results) + 1)
-        ]
-
-        precision_sum = 0
-        for idx in relevant_index:
-            precision_sum = precision_sum + precision_values[idx]
-        
-        return precision_sum / len(relevant_index)
-
-    @metric
-    def p10(results, relevant, n=10):
-        """Precision at N"""
-        return len([
-            doc 
-            for doc in results[:n] 
-            if (i != 2 and doc['name'] in relevant) or (i == 2 and doc['name'][0] in relevant)
-        ])/len(results)
-
-    @metric
-    def r10(results, relevant, n=10):
-        """Recall at N"""
-        return len([
-            doc 
-            for doc in results[:n] 
-            if (i != 2 and doc['name'] in relevant) or (i == 2 and doc['name'][0] in relevant)
-        ])/len(relevant)
-
-
-    def calculate_metric(key, results, relevant):
-        return metrics[key](results, relevant)
-
-    # Define metrics to be calculated
-    evaluation_metrics = {
-        'ap': 'Average Precision',
-        'p10': 'Precision at 10 (P@10)',
-        'r10': 'Recall at 10 (R@10)',
-    }
-
-    # Calculate all metrics and export results as LaTeX table
-    df = pd.DataFrame([['Metric','Value']] +
-        [
-            [evaluation_metrics[m], calculate_metric(m, results, relevant)]
-            for m in evaluation_metrics
-        ]
-    )
-
-    if i == 0:
-        filename = 'results_normal.tex'
-    elif i == 1:
-        filename = 'results_weight.tex'
-    else:
-        filename = 'results_no_schema.tex'
-
-    with open(filename,'w') as tf:
-        tf.write(df.to_latex())
-
-    # In a third step, we calculate the Precision-Recall curve for the query results, to provide
-    # a more visual clue about the systems' performance.
-    # PRECISION-RECALL CURVE
-    precision_values = [
-        len([
-            doc 
-            for doc in results[:idx]
-            if (i != 2 and doc['name'] in relevant) or (i == 2 and doc['name'][0] in relevant)
-        ]) / idx 
-        for idx, _ in enumerate(results, start=1)
-    ]
-
-    recall_values = [
-        len([
-            doc for doc in results[:idx]
-            if (i != 2 and doc['name'] in relevant) or (i == 2 and doc['name'][0] in relevant)
-        ]) / len(relevant)
-        for idx, _ in enumerate(results, start=1)
-    ]
-    
-    if len(precision_values) == 0 or len(recall_values) == 0:
-        continue
-
-    precision_recall_match = {k: v for k,v in zip(recall_values, precision_values)}
-
-    # Extend recall_values to include traditional steps for a better curve (0.1, 0.2 ...)
-    recall_values.extend([step for step in np.arange(0.1, 1.1, 0.1) if step not in recall_values])
-    recall_values = sorted(set(recall_values))
-
-    # Extend matching dict to include these new intermediate steps
-    for idx, step in enumerate(recall_values):
-        if step not in precision_recall_match:
-            if recall_values[idx-1] in precision_recall_match:
-                precision_recall_match[step] = precision_recall_match[recall_values[idx-1]]
-            else:
-                precision_recall_match[step] = precision_recall_match[recall_values[idx+1]]
-
-    disp = PrecisionRecallDisplay([precision_recall_match.get(r) for r in recall_values], recall_values)
-
-    if(i == 0):
-        disp.plot(ax=ax, name=f"Precision-recall without weights",
-            color=color, linewidth=1)
-    elif(i == 1):
-        disp.plot(ax=ax, name=f"Precision-recall with weights",
-                  color=color, linewidth=1.5)
-    elif(i == 2):
-        disp.plot(ax=ax, name=f"Precision-recall without schema",
-            color=color, linewidth=1)
-    i += 1
-
-# add the legend for the iso-f1 curves
-handles, labels = disp.ax_.get_legend_handles_labels()
-
-# set the legend and the axes
-ax.legend(handles=handles, labels=labels, loc="best")
-ax.set_title("Precision-Recall curve of information retrieval 4")
-
-plt.savefig('precision_recall.png')
+get_metrics(True, QRELS_FILE, QUERY_URL, QUERY_URL_WEIGHT)
+get_metrics(False, QRELS_FILE, QUERY_URL, QUERY_URL_WEIGHT)
