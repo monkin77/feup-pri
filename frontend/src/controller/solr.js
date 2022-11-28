6cf1dd8ab4cd1a5c220c9507b0a7ee56d1d7e4ba
João Mesquita <monkinsmurf@gmail.com>
2022-11-28 17:58:12 +0000

    Add CORS policy to accept requests from frontend

--- frontend/src/controller/solr.js
+++ frontend/src/controller/solr.js
@@ -1,21 +1,23 @@
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080";
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8983";
 
 /**
  * 
- * @param {Item} item 
+ * @param {string} query 
  * @returns {ok: boolean, data: data, err: string}
  */
-export const setDone = async(item) => {
+export const querySolr = async(query) => {
     try {
-        const response = await fetch(`${BACKEND_URL}/todo/${item.id}/archive`, {
-            method: 'PUT',
+        // TODO: Additional params  &qf=name+industry+description&rows=10
+        const response = await fetch(`${BACKEND_URL}/solr/reviews/select?_=1668256462851&defType=edismax&indent=true&q=${query}&q.op=OR`, {
+            method: 'GET',
         });
 
         if (!response.ok) {
             return { ok: false }
         }
 
-        const data = await response.json();
+        let data = await response.json();
+        data = data.response;
         return { ok: true, data: data };
     } catch (err) {
         return { ok: false, err: err };
