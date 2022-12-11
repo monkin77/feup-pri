cb669fc9a483c001b0837c5eb8ad3e7902ed051a
Monkin <monkinsmurf@gmail.com>
2022-12-11 10:04:33 +0000

    Added cards in Results Page

--- frontend/src/pages/Results.jsx
+++ frontend/src/pages/Results.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { CircularProgress, Typography } from "@mui/material";
 import { useLocation } from "react-router-dom";
-import {querySolr} from '../controller/solr.js';
+import { querySolr } from "../controller/solr.js";
+import { CompanyCard } from "../components/CompanyCard.jsx";
 
 const styles = {
     container: {
@@ -10,9 +11,8 @@ const styles = {
         backgroundColor: "lightBlue",
         boxSizing: "border-box",
         position: "relative",
-        height: "100%",
     },
-    results: {
+    loadingContainer: {
         display: "flex",
         flexDirection: "column",
         flex: 1,
@@ -22,7 +22,14 @@ const styles = {
     },
     loading: {
         marginBottom: "2rem",
-    }
+    },
+    resultsContainer: {
+        display: "flex",
+        flexDirection: "column",
+        flex: 1,
+        minHeight: "100%",
+    },
+    cardsColumn: { width: "40%" },
 };
 
 export const ResultsPage = () => {
@@ -30,35 +37,51 @@ export const ResultsPage = () => {
     const searchValue = location.state?.searchValue;
 
     const [loading, setLoading] = useState(true);
-    const [results, setResults] = useState({numFound: 0, docs: [], numFoundExact: false, start: 0});
+    const [results, setResults] = useState({
+        numFound: 0,
+        docs: [],
+        numFoundExact: false,
+        start: 0,
+    });
 
     useEffect(() => {
-        // TODO: Fetch items from the backend for this search
-        fetchResults();
-    }, []);
+        const fetchResults = async () => {
+            console.log("Fetching results for: " + searchValue);
+            const res = await querySolr(searchValue);
+            if (!res.ok) {
+                console.log("Error fetching results: " + res);
+            } else {
+                setResults(res.data);
+                setLoading(false);
+            }
+        };
 
-    const fetchResults = async () => {
-        console.log("Fetching results for: " + searchValue);
-        const res = await querySolr(searchValue);
-        if (!res.ok) {
-            console.log("Error fetching results: " + res);
-        } else {
-            setResults(res.data);
-            setLoading(false);
-        }
-    }
+        fetchResults();
+    }, [searchValue]);
 
     return (
         <div style={styles.container}>
-            <Typography variant="h4">Showing Results for: {searchValue}</Typography>
-           
-            {loading ? 
-                    <div style={styles.results}>
-                        <CircularProgress style={styles.loading} size={75} /> 
-                        <Typography variant="h3">Loading...</Typography>
-                    </div> : (
-                <div> 
-                    <Typography variant="h5">Results</Typography>
+            <Typography variant="h4">
+                Showing Results for: {searchValue}
+            </Typography>
+
+            {loading ? (
+                <div style={styles.loadingContainer}>
+                    <CircularProgress style={styles.loading} size={75} />
+                    <Typography variant="h3">Loading...</Typography>
+                </div>
+            ) : (
+                <div style={styles.resultsContainer}>
+                    <div style={styles.cardsColumn}>
+                        {results.docs.map((document) => {
+                            return (
+                                <CompanyCard
+                                    company={document}
+                                    key={document.id}
+                                />
+                            );
+                        })}
+                    </div>
                 </div>
             )}
         </div>
