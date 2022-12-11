80cffba2a840b3ab2fcea7e57af6f93261772ef6
Monkin <monkinsmurf@gmail.com>
2022-12-11 19:27:39 +0000

    Started filters

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Autocomplete, IconButton, InputAdornment, TextField } from "@mui/material";
-import { Search as SearchIcon } from "@mui/icons-material";
+import {
+    Autocomplete,
+    IconButton,
+    InputAdornment,
+    TextField,
+    Typography,
+} from "@mui/material";
+import { Search as SearchIcon, Tune as TuneIcon } from "@mui/icons-material";
 import { Stylesheet } from "../styles/stylesheet";
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 import { getSuggestion } from "../controller/solr";
 
 const styles = {
@@ -29,17 +35,28 @@ const styles = {
     searchBar: {
         width: "40%",
     },
+    searchContainer: {
+        display: "flex",
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "center",
+    },
+    tuneIcon: {
+        marginLeft: 5,
+    },
+    weigthField: {
+        display: "flex",
+        flexDirection: "row",
+    },
 };
-
 
 export const HomePage = () => {
     const [search, setSearch] = useState("");
     const [suggestions, setSuggestions] = useState([]);
+    const [configOpen, setConfigOpen] = useState(false);
     const navigate = useNavigate();
 
-    useEffect(() => {
-        
-    }, []);
+    useEffect(() => {}, []);
 
     const onChangeSearch = async (event) => {
         setSearch(event.target.value);
@@ -47,51 +64,91 @@ export const HomePage = () => {
         if (newSuggestions.ok) {
             setSuggestions(newSuggestions.data);
         }
-    }
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        
-        navigate("/results", { state: {searchValue: search} });
+
+        navigate("/results", { state: { searchValue: search } });
     };
+
+    const toggleConfig = () => setConfigOpen((prevValue) => !prevValue);
 
     return (
         <>
             <div style={styles.container}>
                 <div style={styles.centered}>
                     <h1 style={styles.title}>IndWish</h1>
-                    <form style={styles.searchBar} onSubmit={handleSubmit}>
-                        <Autocomplete
-                            disablePortal
-                            id="combo-box-demo"
-                            options={suggestions}
-                            style={Stylesheet.fullWidth}
-                            onChange={(event, value) => setSearch(value)}
-                            renderInput={(params) =>
-                                <TextField
-                                    {...params}
-                                    id="searchBar"
-                                    label="Search for Companies"
-                                    variant="outlined"
-                                    InputProps={{
-                                        ...params.InputProps,
-                                        autoComplete: 'new-password', // disable autocomplete and autofill
-                                        endAdornment: (
-                                            <InputAdornment position="end">
-                                                <IconButton onClick={handleSubmit}>
-                                                    <SearchIcon />
-                                                </IconButton>
-                                            </InputAdornment>
-                                        ),
-                                    }}
-                                    placeholder="Search"
-                                    color="primary"
-                                    value={search}
-                                    onChange={onChangeSearch}
+
+                    <div style={styles.searchContainer}>
+                        <form style={styles.searchBar} onSubmit={handleSubmit}>
+                            <Autocomplete
+                                disablePortal
+                                id="combo-box-demo"
+                                options={suggestions}
+                                style={Stylesheet.fullWidth}
+                                onChange={(event, value) => setSearch(value)}
+                                renderInput={(params) => (
+                                    <TextField
+                                        {...params}
+                                        id="searchBar"
+                                        label="Search for Companies"
+                                        variant="outlined"
+                                        InputProps={{
+                                            ...params.InputProps,
+                                            autoComplete: "new-password", // disable autocomplete and autofill
+                                            endAdornment: (
+                                                <InputAdornment position="end">
+                                                    <IconButton
+                                                        onClick={handleSubmit}
+                                                    >
+                                                        <SearchIcon />
+                                                    </IconButton>
+                                                </InputAdornment>
+                                            ),
+                                        }}
+                                        placeholder="Search"
+                                        color="primary"
+                                        value={search}
+                                        onChange={onChangeSearch}
                                     />
-                                }
+                                )}
                             />
-                    </form>
+                        </form>
+
+                        <IconButton
+                            style={styles.tuneIcon}
+                            onClick={toggleConfig}
+                        >
+                            <TuneIcon />
+                        </IconButton>
+                    </div>
+
+                    {configOpen && (
+                        <div
+                            style={{
+                                width: "50%",
+                                marginTop: 20,
+                                backgroundColor: "lightGray",
+                                padding: 20,
+                                borderRadius: 20,
+                            }}
+                        >
+                            <Typography>Term Boosting</Typography>
+                            <div style={styles.weigthField}>
+                                <Typography>Name</Typography>
+                                {/* Add slider for boosting */}
+                            </div>
+
+                            <div style={styles.weigthField}>
+                                <Typography>Industry</Typography>
+                            </div>
+
+                            <div style={styles.weigthField}>
+                                <Typography>Descrition</Typography>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
         </>
